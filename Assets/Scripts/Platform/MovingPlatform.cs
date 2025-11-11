c4294f0037b17c09c3f276b2dbdd2f7635ce9140
EoH <zien33195@gmail.com>
2025-11-11 21:53:18 +0900

    feat: 활동발판

---
+++ Assets/Scripts/Platform/MovingPlatform.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    // z축(파란 화살표)을 따라 앞뒤로 움직이는 플랫폼
+    [Header("Move Settings")]
+    public float moveDistance = 5f;      // 앞뒤로 이동할 거리
+    public float moveSpeed = 2f;         // 이동 속도
+    public float waitTime = 0.5f;        // 끝점에서 잠시 멈추는 시간
+
+    private Vector3 startPos;
+    private Vector3 endPos;
+    private bool playerOnPlatform = false;
+    private bool isMoving = false;
+    private Transform player;
+
+    void Start()
+    {
+        startPos = transform.position;
+        endPos = startPos + transform.forward * moveDistance; // 플랫폼의 앞 방향 기준
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            playerOnPlatform = true;
+            player = collision.transform;
+            if (!isMoving) player.SetParent(transform);
+        }
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player") && !isMoving)
+        {
+            playerOnPlatform = false;
+            player.SetParent(null);
+            player = null;
+        }
+    }
+
+    void Update()
+    {
+        // E키 입력 처리
+        if (playerOnPlatform && !isMoving && Input.GetKeyDown(KeyCode.E))
+        {
+            StartCoroutine(MoveRoutine());
+        }
+    }
+
+    IEnumerator MoveRoutine()
+    {
+        isMoving = true;
+
+        //  이동 중엔 항상 플레이어를 자식으로 고정
+        if (player != null) player.SetParent(transform);
+
+        // 앞으로 이동
+        yield return MoveToPosition(endPos);
+        yield return new WaitForSeconds(waitTime);
+
+        // 뒤로 복귀
+        yield return MoveToPosition(startPos);
+        yield return new WaitForSeconds(waitTime);
+
+        //  이동 끝나면 플레이어 내릴 수 있게 해제
+        if (player != null)
+            player.SetParent(null);
+
+        isMoving = false;
+    }
+
+    IEnumerator MoveToPosition(Vector3 target)
+    {
+        while (Vector3.Distance(transform.position, target) > 0.05f)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, target, moveSpeed * Time.deltaTime);
+            yield return null;
+        }
+    }
+}
--- Assets/Scripts/Player/PlayerInteract.cs
+++ Assets/Scripts/Player/PlayerInteract.cs
@@ -5,59 +5,60 @@ using UnityEngine.InputSystem;
 
 public class PlayerInteract : MonoBehaviour
 {
-    public float detectRange = 7f;
-    public LayerMask itemMask;   // 감지할 레이어 (예: Item)
+    public float interactRange = 7f;
+    public LayerMask interactMask;   // 감지할 레이어 (예: Item)
 
-    [Header("UI 표시")]
-    public Text pickupText;
+    public Text interactText;        // UI 텍스트 (Screen에 띄울 것)
 
-    private ItemPickup targetItem;  // 감지된 아이템
+    private Camera cam;
 
-    // Update is called once per frame
-    void Update()
+    void Start()
     {
-        DetectItem();
+        cam = Camera.main;
+        interactText.gameObject.SetActive(false);
     }
 
-    void DetectItem()
+    void Update()
     {
-        Camera cam = Camera.main;
-        Ray ray = cam.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2));
+        HandleInteractionRay();
+    }
+
+    void HandleInteractionRay()
+    {
+        Ray ray = new Ray(cam.transform.position, cam.transform.forward);
         RaycastHit hit;
 
-        if (Physics.Raycast(ray, out hit, detectRange, itemMask))
+        if (Physics.Raycast(ray, out hit, interactRange, interactMask))
         {
-            Debug.DrawRay(ray.origin, ray.direction * hit.distance, Color.green);
-            targetItem = hit.collider.GetComponent<ItemPickup>();
-
-            if (targetItem != null)
+            //  아이템
+            if (hit.collider.CompareTag("Item"))
             {
-                pickupText.text = $"[E] {targetItem.itemData.itemName} 줍기";
-                pickupText.enabled = true;
+                interactText.text = "Press [E] to pick up";
+                interactText.gameObject.SetActive(true);
+
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    ItemPickup pickup = hit.collider.GetComponent<ItemPickup>();
+                    if (pickup != null)
+                    {
+                        pickup.PickUp();  // 아이템 습득
+                    }
+                }
+            }
+            //  플랫폼 (MovingPlatform_Interact)
+            else if (hit.collider.GetComponent<MovingPlatform>() != null)
+            {
+                interactText.text = "Press [E] to move platform";
+                interactText.gameObject.SetActive(true);
             }
             else
             {
-                pickupText.enabled = false;
+                interactText.gameObject.SetActive(false);
             }
         }
         else
         {
-            Debug.DrawRay(ray.origin, ray.direction * detectRange, Color.red);
-            targetItem = null;
-            pickupText.enabled = false;
-        }
-    }   
-    
-
-    public void OnInteract(InputAction.CallbackContext context)
-    {
-        if (context.performed && targetItem != null)
-        {
-            
-            Debug.Log($" {targetItem.itemData.itemName} 획득!");
-            targetItem.PickUp();
-            pickupText.enabled = false;
-            targetItem = null;
+            interactText.gameObject.SetActive(false);
         }
     }
 }
